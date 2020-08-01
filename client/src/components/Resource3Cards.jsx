151425bb229b54cc1c093b249e5fe7cf633ebd13
DeveloperM <mgdevelopertor@gmail.com>
2020-08-01 15:21:09 -0400

    Co-authored-by: Adrian M. <adrianmarimon1@gmail.com>

---
+++ client/src/components/Resource3Cards.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { CardDeck, Card } from 'react-bootstrap';
+import '../assets/styles/resources.css';
+
+function Resource3Cards(props) {
+  return (
+    <CardDeck className="cards">
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </CardDeck>
+  );
+}
+export default Resource3Cards;
--- client/src/components/ResourceCardDeck.jsx
+++ client/src/components/ResourceCardDeck.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CardDeck, Card } from 'react-bootstrap';
+import { CardDeck, Card, Button } from 'react-bootstrap';
 
 const ResourceCardDeck = () => {
   return (
@@ -25,6 +25,21 @@ const ResourceCardDeck = () => {
           <Card.Text>
             This card has supporting text below as a natural lead-in to
             additional content.{' '}
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
