8079837e52b101905beff64d30778d96117b0c5e
Jeannie Torres <jeanniet89@gmail.com>
2020-08-05 14:19:11 -0400

    forgotten merged files

--- client/src/components/HomePgJumbo.jsx
+++ client/src/components/HomePgJumbo.jsx
@@ -4,28 +4,16 @@ import Button from 'react-bootstrap/Button';
 
 function HomePgJumbo() {
   return (
-<<<<<<< HEAD
     <Jumbotron fluid className="homejumbo">
       <Container className="homejumbotext">
-      <div className="container">
-        <h1> LEARN TO CODE</h1>
-        <p>
-          This is a modified jumbotron that occupies the entire horizontal space
-          of its parent.
-        </p>
-        <p>
-          <Button id="button" variant="outline-primary" href="/resources">Resources</Button>
-=======
-    <Jumbotron id="homejumbo">
-      <Container className="hometext">
-        <h1> Want To Learn How To Code?</h1>
-        <p>Click below to find some of the best resources to learn coding.</p>
-        <p>
-          <Button id="homebutton" href="/resources">
-            Resources
-          </Button>
->>>>>>> master
-        </p>
+        <div className="container">
+          <h1> Want To Learn How To Code?</h1>
+          <p>Click below to find some of the best resources to learn coding.</p>
+          <p>
+            <Button id="button" variant="outline-primary" href="/resources">
+              Resources
+            </Button>
+          </p>
         </div>
       </Container>
     </Jumbotron>
